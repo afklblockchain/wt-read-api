7540aeffe913d6b1cec24a4323da3c47d767e4f5
alicedevries <alice-de.vries@klmc.om>
2018-10-25 13:34:56 +0200

    added Lufthansa

--- src/controllers/flight-offers.js
+++ src/controllers/flight-offers.js
@@ -1,4 +1,4 @@
-const NDC = require('ndc-adapter');
+const {NDC_ADAPTER_AFKL, NDC_ADAPTER_LH} = require('ndc-adapter');
 const parser = require('xml2json');
 const { Http404Error } = require('../errors');
 
@@ -46,8 +46,42 @@ const mapOffer = function (ndcOffer, flights, segments, od) {
   return offer;
 };
 
-const mapToJSON = function (offersInXML) {
-  let response = parser.toJson(offersInXML, { object: true })['S:Envelope']['S:Body'].AirShoppingRS;
+const mapOfferLH = function (ndcOffer, segments) {
+  let legs =[];
+
+  for (let segment of  ndcOffer.PricedOffer.Associations.ApplicableFlight.FlightSegmentReference){
+    for (let s of segments) {
+      if (s.SegmentKey === segment.ref) {
+          let leg = {
+              departureStation: s.Departure.AirportCode,
+              arrivalStation: s.Arrival.AirportCode,
+              departureDate: s.Departure.Date,
+              arrivalDate: s.Arrival.Date,
+              departureTime: s.Departure.Time,
+              arrivalTime:  s.Arrival.Time,
+              airlineCode: s.MarketingCarrier.AirlineID,
+              flightNumber: s.MarketingCarrier.FlightNumber,
+          };
+          legs.push(leg);
+      }
+    }
+  }
+
+  let offer = {
+    id: ndcOffer.OfferID.t,
+    price: ndcOffer.TotalPrice.DetailCurrencyPrice.Total.t,
+    currency: ndcOffer.TotalPrice.DetailCurrencyPrice.Total.Code,
+    origin: legs[0].departureStation,
+    destination: legs[legs.length-1].arrivalStation,
+    segments: legs,
+  };
+
+  return offer;
+};
+
+
+const mapAFKL = function (json){
+  let response = json['S:Envelope']['S:Body'].AirShoppingRS;
   let offers = response.OffersGroup.AirlineOffers.Offer;
   let flights = response.DataLists.FlightList.Flight;
   let segments = response.DataLists.FlightSegmentList.FlightSegment;
@@ -58,14 +92,46 @@ const mapToJSON = function (offersInXML) {
     mappedOffers.push(mapOffer(offer, flights, segments, odlist));
   }
   return mappedOffers;
+}
+
+const mapLH = function (json){
+  let response = json.FaresResponse.AirShoppingRS;
+  let offers = response.OffersGroup.AirlineOffers.AirlineOffer;
+  let segments = response.DataLists.FlightSegmentList.FlightSegment;
+
+  let mappedOffers = [];
+  for (let offer of offers) {
+    mappedOffers.push(mapOfferLH(offer, segments));
+  }
+  return mappedOffers;
+}
+
+const mapToJSON = function (offersInXML) {
+  let json = parser.toJson(offersInXML, { object: true });
+  if (json['S:Envelope']){
+    return mapAFKL(json);
+  } else {
+    return mapLH(json);
+  }
 };
 
 const findAll = async (req, res, next) => {
   const { destination, origin, date } = req.query;
   try {
     const baseURL = await res.locals.wt.airline._dataUri;
-    let ndc = new NDC(baseURL+'/passenger/distribmgmt/001448v01/EXT?', '3nnfesjhfupgh9dbb42yay55');
-    let offers = await ndc.AirShopping(origin, destination, date);
+
+    let offers = {}
+    if (baseURL.includes('airfranceklm')) {
+      let ndc = new NDC_ADAPTER_AFKL(baseURL + '/passenger/distribmgmt/001448v01/EXT?', '3nnfesjhfupgh9dbb42yay55');
+      offers = await ndc.AirShopping(origin, destination, date);
+    } else {
+      let ndc = new NDC_ADAPTER_LH(baseURL+'?', 'r4r7swg9qfvrjg33kcc3s9kb');
+      offers = await ndc.AirShopping(origin, destination, date, '2019-02-04');
+      offers = offers.replace(/(?:\r\n|\r|\n)/g, ' ')
+      offers = offers.replace(/@/g, '')
+      offers = offers.replace(/\$/g, 't')
+
+    }
     res.status(200).json(mapToJSON(offers));
   } catch (e) {
     next(e);
